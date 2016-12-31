f6718a941110043f2671302cf46860ad41bbc52a
Steven Chang <prime_pork@hotmail.com>
2016-12-31 13:07:27 +1000

    #application.js - required jquery, angular, lodash etc etc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,13 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require angular
+//= require angular-ui-router
+//= require angular-devise
+//= require angular-rails-templates
+//= require bootstrap-sprockets
+
+//= require lodash
+//= require restangular
+
 //= require_tree .
