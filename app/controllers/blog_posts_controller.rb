ebaaf82f5c0cb1a881ea4ecafaa81142e24fcdac
Steven Chang <prime_pork@hotmail.com>
2017-01-28 18:21:09 +1000

    #blog - Creating a post from the form works

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -9,6 +9,18 @@ class BlogPostsController < ApplicationController
   end
 
   def create
+    blog_post = BlogPost.new(post_params)
+    blog_post.save
+
+    respond_to do |format|
+      format.json { render :json => blog_post, :status => 200 }
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:blog_post).permit(:description, :image_url, :title, :youtube_url, :date_added)
   end
 
 end
