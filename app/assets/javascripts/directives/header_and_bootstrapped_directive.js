a1fd1b129f72665ce2aa15a75443e8e568af9408
Steven Chang <prime_pork@hotmail.com>
2017-01-31 02:46:05 +1000

    #home - got the banner looking fun and simple like I wanted it

--- app/assets/javascripts/directives/header_and_bootstrapped_directive.js
+++
@@ -1,6 +0,0 @@
-app.directive('headerAndBootstrapped', function NavBar(){
-  return {
-    templateUrl: 'views/layout_header_and_bootstrapped.html',
-    transclude: true
-  }
-})
---
+++ app/assets/javascripts/directives/navbar_and_container_directive.js
@@ -0,0 +1,6 @@
+app.directive('navbarAndContainer', function NavBar(){
+  return {
+    templateUrl: 'views/navbar_and_container.html',
+    transclude: true
+  }
+})
