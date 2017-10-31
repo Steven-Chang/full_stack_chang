443ceb8171e68985ee904520f46a8320e82602e6
Steven Chang <prime_pork@hotmail.com>
2017-10-31 14:01:14 +1000

    #NbaGame and NbaTeam - associations between both have been created

--- app/models/nba_team.rb
+++ app/models/nba_team.rb
@@ -1,2 +1,8 @@
 class NbaTeam < ActiveRecord::Base
+  has_many :home_games, :class_name => "NbaGame", :foreign_key => "home_id"
+  has_many :away_games, :class_name => "NbaGame", :foreign_key => "away_id"
+
+  def games
+    NbaGame.where("home_id = ? or away_id = ?", self.id, self.id)
+  end
 end
