f1c37989d7889dd08ab6e9f05d37613284936a23
Steven Chang <prime_pork@hotmail.com>
2018-07-30 21:09:13 +1000

    #serializer update

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :description, :image_url, :title, :url, :date_added
+  attributes :id, :description, :title, :url, :date_added
 
   has_many :attachments
 end
