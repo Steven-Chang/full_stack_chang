19769644187c7a4d16857852a5bb28c4b58aba27
Steven Chang <prime_pork@hotmail.com>
2018-07-31 18:23:43 +1000

    #projects - change from date_added to start_date and change column type from datetime to date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180730112315) do
+ActiveRecord::Schema.define(version: 20180731082032) do
 
   create_table "attachments", force: :cascade do |t|
     t.string "resource_type", null: false
@@ -110,7 +110,7 @@ ActiveRecord::Schema.define(version: 20180730112315) do
     t.text "description"
     t.string "title", null: false
     t.string "url"
-    t.datetime "date_added"
+    t.date "start_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "end_date"
