0b24c60c90c9f5881857b51feea4a9e2083be33f
Steven Chang <prime_pork@hotmail.com>
2018-07-31 18:28:43 +1000

    #project serializer - added the start_date and end_date

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :description, :title, :url, :date_added
+  attributes :id, :description, :title, :url, :start_date, :end_date
 
   has_many :attachments
 end
