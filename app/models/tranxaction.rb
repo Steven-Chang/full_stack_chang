347dc5a58ece8d67538100d0fc0911c9618ba70d
Steven Chang <prime_pork@hotmail.com>
2018-08-07 19:29:57 +1000

    #Tranxaction.rb - after a client tranxaction is created, a work tranxactable is created automatically

--- app/models/tranxaction.rb
+++ app/models/tranxaction.rb
@@ -1,4 +1,17 @@
 class Tranxaction < ApplicationRecord
   has_many :tranxactables
   has_many :attachments, as: :resource, dependent: :destroy
+
+  after_create do |tranxaction|
+    tranxaction.tranxactables.each do |tranxactable|
+      if tranxactable.resource_type == "Client"
+        # We want to build the work tranxactable
+        new_tranxactable = Tranxactable.new
+        new_tranxactable.resource_type = "TranxactionType"
+        new_tranxactable.resource_id = TranxactionType.find_by(description: "work").id
+        new_tranxactable.tranxaction_id = tranxaction.id
+        new_tranxactable.save
+      end
+    end
+  end
 end
