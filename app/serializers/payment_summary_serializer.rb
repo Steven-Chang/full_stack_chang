1740fb11d79edcb69f454ac641b036a577f64f03
Steven Chang <prime_pork@hotmail.com>
2018-08-09 09:43:28 +1000

    #clients.show - payment summaries attachments are now showing

--- app/serializers/payment_summary_serializer.rb
+++ app/serializers/payment_summary_serializer.rb
@@ -1,3 +1,3 @@
 class PaymentSummarySerializer < ActiveModel::Serializer
-  attributes :id, :gross_payment, :total_tax_withheld, :year_ending, :total_allowances, :client_id
+  attributes :id, :gross_payment, :total_tax_withheld, :year_ending, :total_allowances, :client_id, :attachments
 end
