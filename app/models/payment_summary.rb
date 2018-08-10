4afac910e8c945552547b2c44fb3ffae897ad69c
Steven Chang <prime_pork@hotmail.com>
2018-08-10 20:50:13 +1000

    #payment_summary - added gross_payment method to payment_summary and added it to serializer too

--- app/models/payment_summary.rb
+++ app/models/payment_summary.rb
@@ -4,4 +4,9 @@ class PaymentSummary < ApplicationRecord
   has_many :attachments, as: :resource, dependent: :destroy
 
   validates_uniqueness_of :client_id, scope: :year_ending
+
+  def gross_payment
+    Client.find( self.client_id ).tranxactions.where(tax: true).where("amount > ?", 0).where("date >= ?", Date.new( self.year_ending - 1, 7, 1) ).where("date < ?", Date.new( self.year_ending, 7, 1 ))
+  end
+
 end
--- app/serializers/payment_summary_serializer.rb
+++ app/serializers/payment_summary_serializer.rb
@@ -1,3 +1,3 @@
 class PaymentSummarySerializer < ActiveModel::Serializer
-  attributes :id, :total_tax_withheld, :year_ending, :total_allowances, :client_id, :attachments
+  attributes :id, :total_tax_withheld, :year_ending, :total_allowances, :client_id, :attachments, :gross_payment
 end
