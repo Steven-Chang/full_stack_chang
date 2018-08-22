d84e22613018429da16d55a54c831e16ca9a3fb3
Steven Chang <prime_pork@hotmail.com>
2018-08-22 16:25:54 +1000

    #application.rb - removing this must belong_to bullshit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180822060932) do
+ActiveRecord::Schema.define(version: 20180822061459) do
 
   create_table "aims", force: :cascade do |t|
     t.string "description", null: false
@@ -207,6 +207,7 @@ ActiveRecord::Schema.define(version: 20180822060932) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "tax"
+    t.integer "tax_category_id"
   end
 
   create_table "users", force: :cascade do |t|
