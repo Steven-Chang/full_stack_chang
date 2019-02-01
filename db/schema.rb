7e9826c97292b283a8ed0eb2eb38aca712665d3d
Steven Chang <prime_pork@hotmail.com>
2019-02-01 22:58:11 +1100

    add private to blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190130205821) do
+ActiveRecord::Schema.define(version: 20190201115609) do
 
   create_table "aims", force: :cascade do |t|
     t.string "description", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20190130205821) do
     t.datetime "date_added"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "private", default: true
   end
 
   create_table "blog_posts_tags", id: false, force: :cascade do |t|
