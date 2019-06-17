cf45f504cbae693bbc0b708839719a0d5ff3ff95
Steven Chang <steven@chilfundraising.com>
2019-06-18 09:17:32 +1000

    Rubocop and clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,50 +1,37 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :tax_categories
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  resources :aims
+  # Attachments
+  get 'attachments/presigned' => 'attachments#presigned'
+  resources :attachments
+  resources :blog_posts, only: %i[index create destroy]
+  resources :creditors
+  resources :client_payments
+  resources :clients
   get 'entries/by_date' => 'entries#by_date'
   resources :entries
-  resources :aims
-  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
-  root 'application#index'
-
-  # Attachments
-  get "attachments/presigned" => "attachments#presigned"
-  resources :attachments
-
-  get "payment_summaries/year_endings" => "payment_summaries#year_endings"
+  get 'jobs/balance' => 'jobs#balance'
+  resources :jobs
+  get 'payment_summaries/year_endings' => 'payment_summaries#year_endings'
   resources :payment_summaries
-
-  get 'tranxactions/balance' => 'tranxactions#balance'
-  resources :tranxactions
-
-  resources :tranxaction_types
-
+  resources :projects
   resources :properties do
     resources :tenancy_agreements
   end
-
-  # Tenancy Agreements
-  get "tenancy_agreements/summaries" => "tenancy_agreements#summaries"
-  resources :tenancy_agreements
-  resources :creditors
-  resources :client_payments
-
-  devise_for :users, controllers: { 
-    registrations: "users/registrations"
-  }
-
-  resources :cleaning_records, only: [:index, :create, :destroy]
-  resources :cleaning_tasks, only: [:index, :create, :destroy]
-  resources :clients
-  resources :farming_transactions, except: [:show]
-
-  get 'jobs/balance' => 'jobs#balance'
-  resources :jobs
-
-  resources :projects
-  resources :scores, only: [:index, :create]
-  resources :blog_posts, only: [:index, :create, :destroy]
   resources :rent_transactions, except: [:show]
-
+  resources :scores, only: %i[index create]
+  resources :tax_categories
+  # Tenancy Agreements
+  get 'tenancy_agreements/summaries' => 'tenancy_agreements#summaries'
+  resources :tenancy_agreements
+  get 'tranxactions/balance' => 'tranxactions#balance'
+  resources :tranxactions
+  resources :tranxaction_types
   # Users
   get 'users/:user_id/bond' => 'users#bond'
   get 'users/:user_id/balance' => 'users#balance'
@@ -53,61 +40,8 @@ Rails.application.routes.draw do
   get 'tenants' => 'users#tenants'
   resources :users, only: [:index]
 
+  root 'application#index'
+
  # Need this for prettifying url
-  get '/', :to => redirect('/#!/')
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get '/', to => redirect('/#!/')
 end
