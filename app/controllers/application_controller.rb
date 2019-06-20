64688aab758f1955a91768785f971c0d38e81312
Steven Chang <steven@chilfundraising.com>
2019-06-21 06:10:05 +1000

    Rubocop clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -12,12 +14,19 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:username, :email, :password, :tenant, :first_name, :last_name]
+    added_attrs = %i[
+      username
+      email
+      password
+      tenant
+      first_name
+      last_name
+    ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 
   def authenticate_admin
-    redirect_to root_path unless current_user && current_user.admin
+    redirect_to root_path unless current_user && current_user&.admin
   end
 end
