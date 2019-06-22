0b3784e6f6f0c9d51949be281c99a8986f0e198a
Steven Chang <steven@chilfundraising.com>
2019-06-22 16:18:56 +1000

    Upgrade to rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -9,20 +11,13 @@ Bundler.require(*Rails.groups)
 module FullStackChang
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     config.active_record.belongs_to_required_by_default = false
     config.assets.paths << Rails.root.join('public')
 
     config.to_prepare do
       DeviseController.respond_to :html, :json
-    end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.generators do |g|
-      g.assets false
     end
   end
 end
