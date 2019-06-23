c8d7f924a5da989e3171916fd23c965323fdc57c
Steven Chang <steven@chilfundraising.com>
2019-06-23 16:29:05 +1000

    Rubocop attachment model

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class Attachment < ApplicationRecord
   belongs_to :resource, polymorphic: true
 
   after_destroy do |attachment|
-    Attachment.aws_client.delete_object({
-        bucket: ENV['BUCKET_NAME'],
-        key: attachment.aws_key
-    })
-    # No idea if amazon returns a success or fail?? 
+    # No idea if amazon returns a success or fail??
     # Most of the net seems to suggest it doesn't
+    Attachment.aws_client.delete_object(
+      bucket: ENV['BUCKET_NAME'],
+      key: attachment.aws_key
+    )
   end
 
   def self.aws_client
@@ -15,7 +17,7 @@ class Attachment < ApplicationRecord
   end
 
   def self.aws_resource
-    creds = Aws::Credentials.new( ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'] )
-    Aws::S3::Resource.new( region: 'ap-southeast-2', credentials: creds )
+    creds = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+    Aws::S3::Resource.new(region: 'ap-southeast-2', credentials: creds)
   end
 end
