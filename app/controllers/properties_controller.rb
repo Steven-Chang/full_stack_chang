964ca79820b523e3f147df81c4321ca3c2dab031
Steven Chang <steven@chilfundraising.com>
2019-06-26 09:30:12 +1000

    Rubocop properties controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,43 +1,34 @@
-class PropertiesController < ApplicationController
-  before_action :set_property, only: [:show, :edit, :update, :destroy]
+# frozen_string_literal: true
 
-  # GET /properties
-  # GET /properties.json
+class PropertiesController < ApplicationController
+  before_action :set_property, only: %i[show edit update destroy]
+
   def index
     @properties = Property.all
 
-    render :json => @properties
+    render json: @properties
   end
 
-  # GET /properties/1
-  # GET /properties/1.json
   def show
-    render :json => @property
+    render json: @property
   end
 
-  # GET /properties/new
   def new
     @property = Property.new
   end
 
-  # GET /properties/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /properties
-  # POST /properties.json
   def create
     @property = Property.new(property_params)
 
     if @property.save
-      render :json => @property
+      render json: @property
     else
-      render :json => @property.errors, status: :unprocessable_entity
+      render json: @property.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /properties/1
-  # PATCH/PUT /properties/1.json
   def update
     respond_to do |format|
       if @property.update(property_params)
@@ -50,8 +41,6 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # DELETE /properties/1
-  # DELETE /properties/1.json
   def destroy
     @property.destroy
     respond_to do |format|
@@ -61,13 +50,12 @@ class PropertiesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_property
-      @property = Property.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def property_params
-      params.require(:property).permit(:address)
-    end
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:address)
+  end
 end
