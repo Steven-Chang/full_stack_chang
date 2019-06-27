026c485db1bd85028dd611608916c67572d9c99f
Steven Chang <steven@chilfundraising.com>
2019-06-27 14:03:41 +1000

    Set up ScheduledTranxactionTemplate

---
+++ app/admin/scheduled_tranxaction_templates.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+# params['commit'] exists when filters are selected
+ActiveAdmin.register ScheduledTranxactionTemplate do
+  # === INDEX ===
+  index do
+    id_column
+    column :date
+    column :description
+    column :amount do |scheduled_tranxaction_template|
+      number_to_currency(scheduled_tranxaction_template.amount)
+    end
+    column :days_for_recurrence
+    column :enabled
+    column :tax
+    column :tax_category do |scheduled_tranxaction_template|
+      scheduled_tranxaction_template&.tax_category&.description
+    end
+    column 'Tranxactable' do |scheduled_tranxaction_template|
+      next if scheduled_tranxaction_template.tranxactable.blank?
+
+      link_description = "#{scheduled_tranxaction_template.tranxactable.class}: "
+      if scheduled_tranxaction_template.tranxactable.class == TenancyAgreement
+        link_description += scheduled_tranxaction_template.tranxactable.user.username
+      elsif scheduled_tranxaction_template.tranxactable.class == Client
+        link_description += scheduled_tranxaction_template.tranxactable.name
+      elsif scheduled_tranxaction_template.tranxactable.class == Property
+        link_description += scheduled_tranxaction_template.tranxactable.address
+      end
+      url = "/admin/#{scheduled_tranxaction_template.tranxactable.class.to_s.underscore.pluralize}/#{scheduled_tranxaction_template.tranxactable.id}"
+      link_to link_description, url
+    end
+    column :creditor
+    actions
+  end
+
+  # === SHOW ===
+  show do
+    attributes_table do
+      row :id
+      row :date
+      row :description
+      row :amount do |scheduled_tranxaction_template|
+        number_to_currency(scheduled_tranxaction_template.amount)
+      end
+      row :days_for_recurrence
+      row :enabled
+      row :tax
+      row :tax_category do |scheduled_tranxaction_template|
+        scheduled_tranxaction_template&.tax_category&.description
+      end
+      row :tranxactable do |scheduled_tranxaction_template|
+        next if scheduled_tranxaction_template.tranxactable.blank?
+
+        link_description = "#{scheduled_tranxaction_template.tranxactable.class}: "
+        if scheduled_tranxaction_template.tranxactable.class == TenancyAgreement
+          link_description += scheduled_tranxaction_template.tranxactable.user.username
+        elsif scheduled_tranxaction_template.tranxactable.class == Client
+          link_description += scheduled_tranxaction_template.tranxactable.name
+        elsif scheduled_tranxaction_template.tranxactable.class == Property
+          link_description += scheduled_tranxaction_template.tranxactable.address
+        end
+        url = "/admin/#{scheduled_tranxaction_template.tranxactable.class.to_s.underscore.pluralize}/#{scheduled_tranxaction_template.tranxactable.id}"
+        link_to link_description, url
+      end
+      row :creditor
+    end
+  end
+
+  # === FORM ===
+  form do |f|
+    f.object.date ||= Date.current
+    f.inputs do
+      f.input :date, required: true
+      f.input :description, required: true
+      f.input :amount, required: true
+      f.input :days_for_recurrence, required: true
+      f.input :enabled
+      f.input :tax
+      f.input :tax_category, member_label: :description
+      f.input :tranxactable_type, collection: %w[Client Property TenancyAgreement]
+      f.input :tranxactable_id, as: :select, collection: Client.all.map { |client| [client.name, client.id] } + Property.all.map { |property| [property.address, property.id] } + TenancyAgreement.all.map { |tenancy_agreement| [tenancy_agreement.user.username || tenancy_agreement.user.email, tenancy_agreement.id] }
+      f.input :creditor, member_label: :name, collection: Creditor.order('LOWER(name)')
+    end
+    f.actions
+  end
+
+  # === PERMIT PARAMS ===
+  permit_params :amount,
+                :creditor_id,
+                :date,
+                :days_for_recurrence,
+                :description,
+                :enabled,
+                :tax,
+                :tax_category_id,
+                :tranxactable_type,
+                :tranxactable_id
+end
--- app/controllers/tranxactions_controller.rb
+++ app/controllers/tranxactions_controller.rb
@@ -2,44 +2,10 @@
 
 class TranxactionsController < ApplicationController
   before_action :authenticate_admin_user!
-  before_action :set_tranxaction, only: %i[destroy show update]
-
-  def index
-    if params[:resource_type] && params[:resource_id]
-      @tranxactions = params[:resource_type].constantize.find(params[:resource_id]).tranxactions
-    else
-      @tranxactions = Tranxaction.all
-    end
-
-    if params[:tax]&.length > 0
-      @tranxactions = @tranxactions.where(tax: params[:tax] == 'true')
-    end
-
-    render json: @tranxactions.order(date: :desc)
-  end
+  before_action :set_tranxaction, only: %i[show update]
 
   def show
     render json: @tranxaction
-  end
-
-  def create
-    tranxaction = Tranxaction.new(tranxaction_params)
-    Tranxaction.transaction do
-      tranxaction.save
-      params[:tranxactables].each do |tranxactable|
-        Tranxactable.create(resource_type: tranxactable[:resource_type], resource_id: tranxactable[:resource_id], tranxaction_id: tranxaction.id)
-      end
-
-      params[:attachments]&.each do |attachment|
-        Attachment.create(resource_type: 'Tranxaction', resource_id: tranxaction.id, url: attachment[:url], aws_key: attachment[:aws_key])
-      end
-    end
-
-    if tranxaction.persisted?
-      render json: tranxaction
-    else
-      render json: tranxaction.errors
-    end
   end
 
   def update
@@ -47,14 +13,6 @@ class TranxactionsController < ApplicationController
       render json: @tranxaction, status: :ok
     else
       render json: @tranxaction.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    if @tranxaction.destroy
-      render json: { message: 'removed' }, status: :ok
-    else
-      render json: { message: 'Error' }, status: :expectation_failed
     end
   end
 
