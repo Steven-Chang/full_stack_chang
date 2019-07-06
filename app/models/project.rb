01ceb5fe3eccaff5398f2f81d69004eeea5e2a45
Steven Chang <stevenchang5000@gmail.com>
2019-07-06 20:00:20 +1000

    Rubocop and write specs for Project (#37)

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
 class Project < ApplicationRecord
+	# === ASSOCIATIONS ===
   has_many :scores, dependent: :destroy
   has_many :attachments, as: :resource, dependent: :destroy
+
+  # === VALIDATIONS ===
+  validates :title, presence: true
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,25 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  let(:per_page) { 12 }
-  let(:number_of_public_posts) { per_page / 2 }
-  let(:number_of_private_posts) { per_page / 2 }
-  let(:prime_pork_user) { create(:user, email: 'prime_pork@hotmail.com') }
-  let(:user) { create(:user) }
-  let(:project) { create(:project) }
+  describe 'ASSOCIATIONS' do
+    it { should have_many(:scores).dependent(:destroy) }
+    it { should have_many(:attachments).dependent(:destroy) }
+  end
 
-  describe 'attributes' do
-    before do
-      @title = 'Title'
-      @project = create(:project, title: @title)
-    end
-
-    it 'should have a title attribute' do
-      expect(@project.title).to eq(@title)
-    end
-
-    it 'should have a private attribute which defaults to true' do
-      expect(@project.private).to eq(true)
-    end
+  describe 'VALIDATIONS' do
+    it { should validate_presence_of(:title) }
   end
 end
