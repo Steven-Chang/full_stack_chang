1a0c09d1a7617e7253a329a57f675827847c9d2e
Steven Chang <steven@chilfundraising.com>
2019-07-06 21:57:14 +1000

    Add level and lines to score and adjust tetris accordingly

--- app/models/level.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Level < ApplicationRecord
-	# === ASSOCIATIONS ===
-  belongs_to :score
-end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,59 +1,16 @@
+# frozen_string_literal: true
+
 class Score < ApplicationRecord
+	# === ASSOCIATIONS ===
   belongs_to :project
-  has_one :level, dependent: :destroy
-  has_one :line, dependent: :destroy
 
-  def self.create_associations_for_score(associations, score)
-    title = Project.find(score.project_id).title
-
-    create_level_association_for_score(associations["level"], score) if associations["level"]
-    create_line_association_for_score(associations["lines"], score) if associations["lines"]
-  end
-
-  def self.create_level_association_for_score(level, score)
-    new_level = Level.new(:score_id => score.id, :level => level)
-    new_level.save
-    score.level = new_level
-  end
-
-  def self.create_line_association_for_score(lines, score)
-    new_line = Line.new(:score_id => score.id, :lines => lines)
-    new_level.save
-    score.line = new_line
-  end
-
-  def self.create_line_association_for_score(lines, score)
-    new_line = Line.new(:score_id => score.id, :lines => lines)
-    new_line.save
-    score.line = new_line
-  end
-
-  def self.return_high_scores( project_id )
-    Score.where( :project_id => project_id ).order( score: :desc ).limit( 5 )
-  end
-
-  def self.return_high_scores_and_associations( project_id )
-    high_scores = return_high_scores( project_id )
-    title = Project.find( project_id ).title
-
-    if title == "Tetris"
-      return_high_scores_and_associations_for_tetris( high_scores )
-    end
-  end
-
-  def self.return_high_scores_and_associations_for_tetris(high_scores)
-    high_scores_and_associations = []
-
-    high_scores.each do |high_score|
-      high_score_as_object = {}
-      high_score_as_object["score"] = high_score.score
-      high_score_as_object["name"] = high_score.name
-      high_score_as_object["lines"] = high_score.line.lines
-      high_score_as_object["level"] = high_score.level.level
-      high_scores_and_associations.push(high_score_as_object)
-    end
-
-    high_scores_and_associations
-  end
-
+  # === VALIDATIONS ===
+  validates :name, presence: true
+  validates :score, numericality: { only_integer: true }
+  validates :level,
+            :lines,
+            numericality: {
+              only_integer: true,
+              allow_nil: true
+            }
 end
