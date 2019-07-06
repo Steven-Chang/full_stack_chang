d34b79d043d8f21d98d6981e93df4f1461e77bc5
Steven Chang <steven@chilfundraising.com>
2019-07-06 15:48:46 +1000

    Rubocop and write specs for payment summary

--- app/models/payment_summary.rb
+++ app/models/payment_summary.rb
@@ -1,16 +1,30 @@
+# frozen_string_literal: true
+
 class PaymentSummary < ApplicationRecord
-  belongs_to :client
+	# === ASSOCIATIONS ===
+	belongs_to :client
+	has_many :attachments, as: :resource, dependent: :destroy
 
-  has_many :attachments, as: :resource, dependent: :destroy
+	# === VALIDATIONS ===
+	validates :client_id, uniqueness: { scope: :year_ending }
+	validates :year_ending, numericality: { only_integer: true }
 
-  validates_uniqueness_of :client_id, scope: :year_ending
-
-  def gross_payment
-    Client.find( self.client_id ).tranxactions.where(tax: true).where("amount > ?", 0).where("date >= ?", Date.new( self.year_ending - 1, 7, 1) ).where("date < ?", Date.new( self.year_ending, 7, 1 )).sum(:amount)
-  end
+  # === INSTANCE METHODS ===
+	def gross_payment
+    client.tranxactions
+          .where(tax: true)
+          .where('amount > ?', 0)
+          .where('date >= ?', Date.new(year_ending - 1, 7, 1))
+          .where('date < ?', Date.new(year_ending, 7, 1))
+          .sum(:amount)
+	end
 
   def total_expenses
-    Client.find( self.client_id ).tranxactions.where(tax: true).where("amount < ?", 0).where("date >= ?", Date.new( self.year_ending - 1, 7, 1) ).where("date < ?", Date.new( self.year_ending, 7, 1 )).sum(:amount)
+    client.tranxactions
+          .where(tax: true)
+          .where('amount < ?', 0)
+          .where('date >= ?', Date.new(year_ending - 1, 7, 1))
+          .where('date < ?', Date.new(year_ending, 7, 1))
+          .sum(:amount)
   end
-
 end
