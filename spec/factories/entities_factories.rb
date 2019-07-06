10164ac5c35615bfabf6dd755e8f81cffbd7f12a
Steven Chang <stevenchang5000@gmail.com>
2019-07-06 16:49:34 +1000

    Rubocop and write specs for payment summary (#36)

--- spec/factories/entities_factories.rb
+++ spec/factories/entities_factories.rb
@@ -9,8 +9,25 @@ FactoryBot.define do
     title { 'Blog post title' }
   end
 
+  factory :client do
+    sequence(:name) { |n| "client #{n}" }
+  end
+
+  factory :payment_summary do
+    year_ending { Date.current.month < 7 ? Date.current.year : Date.current.year + 1 }
+
+    association :client
+  end
+
   factory :project do
     title { 'Project title' }
+  end
+
+  factory :tranxaction do
+    date { Date.current }
+    sequence(:description) { |n| "Tranxaction description #{n}" }
+    amount { rand(1..100) }
+    tax { true }
   end
 
   factory :user do
