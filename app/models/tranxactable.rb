c8588ffd67e8212c37ddc2aa7f57e5dcbd7fbf0f
Steven Chang <steven@chilfundraising.com>
2019-07-07 11:37:26 +1000

    Rubocop and write specs for tranxaction

--- app/models/tranxactable.rb
+++ app/models/tranxactable.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class Tranxactable < ApplicationRecord
+	# === ASSOCIATIONS ===
   belongs_to :tranxaction
   belongs_to :resource, polymorphic: true
 end
--- app/models/tranxaction.rb
+++ app/models/tranxaction.rb
@@ -1,18 +1,24 @@
+# frozen_string_literal: true
+
 class Tranxaction < ApplicationRecord
+  # === ASSOCIATIONS ===
   has_many :tranxactables, dependent: :destroy
   has_many :attachments, as: :resource, dependent: :destroy
   belongs_to :tax_category
 
+  # === CALLBACKS ===
   after_create do |tranxaction|
     tranxaction.tranxactables.each do |tranxactable|
-      if tranxactable.resource_type == "Client"
-        # We want to build the work tranxactable
-        new_tranxactable = Tranxactable.new
-        new_tranxactable.resource_type = "TranxactionType"
-        new_tranxactable.resource_id = TranxactionType.find_by(description: "work").id
-        new_tranxactable.tranxaction_id = tranxaction.id
-        new_tranxactable.save
-      end
+      next unless tranxactable.resource_type == 'Client'
+
+      # We want to build the work tranxactable
+      new_tranxactable = Tranxactable.new
+      new_tranxactable.resource_type = 'TranxactionType'
+      new_tranxactable.resource_id = TranxactionType.find_by(description: 'work').id
+      new_tranxactable.tranxaction_id = tranxaction.id
+      new_tranxactable.save
+      # Seeing if this even gets called
+      raise 'error'
     end
   end
 end
