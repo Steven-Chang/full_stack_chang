d918718509849e4d98e7ba83e86bd9e0dc8cf658
Steven Chang <stevenchang5000@gmail.com>
2019-07-07 18:32:57 +1000

    Rubocop projects controller (#44)
    Rubocop and write specs for user (#45)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,12 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # === DEVISE ===
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable #:validatable
+         :recoverable, :rememberable, :trackable
 
-  has_many :cleaning_records, dependent: :destroy
-  has_many :rent_transactions, dependent: :destroy
-  has_many :tenancy_agreements
-  has_many :tranxactables, as: :resource
+  # === ASSOCIATIONS ===
+  has_many :tenancy_agreements, dependent: :restrict_with_error
+  has_many :tranxactables, as: :resource, dependent: :restrict_with_error
   has_many :tranxactions, through: :tranxactables
-
-  def cleaning_summary
-    summary = {}
-    CleaningTask.all.each do |ct|
-      summary[ct.id] = CleaningRecord.where(:user_id => self.id).where(:cleaning_task_id => ct.id).length
-    end
-    summary
-  end
-
-  def the_boss_man?
-    email == 'prime_pork@hotmail.com'
-  end
 end
