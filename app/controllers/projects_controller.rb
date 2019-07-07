b6ef94c5192b8b2185ad1d93fbfc2395143080ea
Steven Chang <stevenchang5000@gmail.com>
2019-07-07 18:32:57 +1000

    Rubocop projects controller (#44)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,23 +1,25 @@
+# frozen_string_literal: true
+
 class ProjectsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
-  before_action :set_project, only: [:destroy, :show, :update]
+  before_action :set_project, only: %i[destroy show update]
 
   def index
-    if current_user&.email == 'prime_pork@hotmail.com'
-      projects = Project.all
+    projects = if current_user&.email == 'prime_pork@hotmail.com'
+      Project.all
     else
-      projects = Project.where(private: false)
+      Project.where(private: false)
     end
 
     current_projects = projects.where(end_date: nil).order('start_date DESC')
     past_projects = projects.where.not(end_date: nil).order('end_date DESC')
     projects = current_projects + past_projects
 
-    render :json => projects, current_user: current_user, status: :ok
+    render json: projects, current_user: current_user, status: :ok
   end
 
   def show
-    render :json => @project
+    render json: @project
   end
 
   def create
@@ -25,45 +27,44 @@ class ProjectsController < ApplicationController
 
     Project.transaction do
       project.save
-
-      params[:attachments].each do |attachment|
-        Attachment.create(resource_type: "Project", resource_id: project.id, url: attachment[:url], aws_key: attachment[:aws_key] )
-      end if params[:attachments]
+      params[:attachments]&.each do |attachment|
+        Attachment.create(resource_type: 'Project',
+                          resource_id: project.id,
+                          url: attachment[:url],
+                          aws_key: attachment[:aws_key])
+      end
     end
 
     if project.persisted?
-      render :json => project
+      render json: project
     else
-      render :json => project.errors
+      render json: project.errors
     end
   end
 
-  # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
   def update
-    if @project.update( project_params )
-      render :json => @project, status: :ok
+    if @project.update(project_params)
+      render json: @project, status: :ok
     else
-      render :json => @project.errors, status: :unprocessable_entity
+      render json: @project.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
     if @project.destroy
-      render json: { message: "Removed" }, status: :ok
+      render json: { message: 'Removed' }, status: :ok
     else
-      render json: { message: "Error" }, status: :expectation_failed
+      render json: { message: 'Error' }, status: :expectation_failed
     end
   end
 
   private
 
   def set_project
-    @project = Project.find( params[:id] )
+    @project = Project.find(params[:id])
   end
 
   def project_params
     params.require(:project).permit(:description, :private, :title, :url, :start_date, :end_date)
   end
-
 end
