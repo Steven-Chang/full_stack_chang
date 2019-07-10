8d4bb54daf25caa793f92ceabe8d96bc6032e165
Steven Chang <stevenchang5000@gmail.com>
2019-07-11 09:56:03 +1000

    Make stevenchang5000 an admin (#54)

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,9 @@ class User < ApplicationRecord
   has_many :tenancy_agreements, dependent: :restrict_with_error
   has_many :tranxactables, as: :resource, dependent: :restrict_with_error
   has_many :tranxactions, through: :tranxactables
+
+  # === INSTANCE METHODS ===
+  def admin?
+  	%w[prime_pork@hotmail.com stevenchang5000@gmail.com].include?(email)
+  end
 end
