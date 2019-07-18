e2a5993bd6a30543960cef1640fe38aa2449e8ff
Steven Chang <stevenchang5000@gmail.com>
2019-07-18 21:13:00 +1000

    Remove tags mention in BlogPostSerializer (#66)

--- app/serializers/blog_post_serializer.rb
+++ app/serializers/blog_post_serializer.rb
@@ -3,6 +3,5 @@
 class BlogPostSerializer < ActiveModel::Serializer
   attributes :id, :description, :image_url, :private, :title, :youtube_url, :date_added
 
-  has_many :tags
   has_many :attachments
 end
