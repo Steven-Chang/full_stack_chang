80cd878525a646084ded2ab282e603474692bc8b
Steven Chang <steven@chilfundraising.com>
2019-07-23 08:36:47 +1000

    Switch angular devise to use admin instead of users

--- vendor/assets/bower_components/angular-devise/lib/devise.js
+++ vendor/assets/bower_components/angular-devise/lib/devise.js
@@ -42,11 +42,11 @@
      * The default paths.
      */
     var paths = {
-        login: '/users/sign_in.json',
-        logout: '/users/sign_out.json',
-        register: '/users.json',
-        sendResetPasswordInstructions: '/users/password.json',
-        resetPassword: '/users/password.json'
+        login: '/admin/sign_in.json',
+        logout: '/admin/sign_out.json',
+        register: '/admin.json',
+        sendResetPasswordInstructions: '/admin/password.json',
+        resetPassword: '/admin/password.json'
     };
 
     /**
@@ -202,7 +202,7 @@
              * A login function to authenticate with the server.
              * Keep in mind, credentials are sent in plaintext;
              * use a SSL connection to secure them. By default,
-             * `login` will POST to '/users/sign_in.json'.
+             * `login` will POST to '/admin/sign_in.json'.
              *
              * The path and HTTP method used to login are configurable
              * using
@@ -239,7 +239,7 @@
 
             /**
              * A logout function to de-authenticate from the server.
-             * By default, `logout` will DELETE to '/users/sign_out.json'.
+             * By default, `logout` will DELETE to '/admin/sign_out.json'.
              *
              * The path and HTTP method used to logout are configurable
              * using
@@ -267,7 +267,7 @@
              * A register function to register and authenticate
              * with the server. Keep in mind, credentials are sent
              * in plaintext; use a SSL connection to secure them.
-             * By default, `register` will POST to '/users.json'.
+             * By default, `register` will POST to '/admin.json'.
              *
              * The path and HTTP method used to login are configurable
              * using
@@ -296,7 +296,7 @@
             /**
              * A function to send the reset password instructions to the
              * user email.
-             * By default, `sendResetPasswordInstructions` will POST to '/users/password.json'.
+             * By default, `sendResetPasswordInstructions` will POST to '/admin/password.json'.
              *
              * The path and HTTP method used to send instructions are configurable
              * using
@@ -320,7 +320,7 @@
 
             /**
              * A reset function to reset user password.
-             * By default, `resetPassword` will PUT to '/users/password.json'.
+             * By default, `resetPassword` will PUT to '/admin/password.json'.
              *
              * The path and HTTP method used to reset password are configurable
              * using
