dc45df6aa77b14622f5596049507bfe11d73a2c2
Steven Chang <steven@chilfundraising.com>
2019-07-31 14:15:22 +1000

    Remove comfy cms stuff

---
+++ db/migrate/20190731035147_remove_comfy_cms_stuff.rb
@@ -0,0 +1,14 @@
+class RemoveComfyCmsStuff < ActiveRecord::Migration[5.2]
+  def change
+  	drop_table :comfy_cms_categories
+		drop_table :comfy_cms_categorizations
+		drop_table :comfy_cms_files
+		drop_table :comfy_cms_fragments
+		drop_table :comfy_cms_layouts
+		drop_table :comfy_cms_pages
+		drop_table :comfy_cms_revisions
+		drop_table :comfy_cms_sites
+		drop_table :comfy_cms_snippets
+		drop_table :comfy_cms_translations
+  end
+end
