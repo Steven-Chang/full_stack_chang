a8613cfa2099ae541ae4ec129b8a33234ed19a33
Steven Chang <steven@chilfundraising.com>
2019-08-04 16:39:45 +1000

    Remove image url from blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_04_062511) do
+ActiveRecord::Schema.define(version: 2019_08_04_063901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2019_08_04_062511) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.text "description"
-    t.string "image_url"
     t.string "title", null: false
     t.string "youtube_url"
     t.datetime "date_added"
