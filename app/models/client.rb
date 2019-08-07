581956c04f60fe9c2e2994c246299d1239665df1
Steven Chang <steven@chilfundraising.com>
2019-08-07 23:15:35 +1000

    Remove tranxactable association on property and client

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,7 +3,6 @@
 class Client < ApplicationRecord
 	# === ASSOCIATIONS ===
   has_many :payment_summaries, dependent: :destroy
-  has_many :tranxactables, as: :resource, dependent: :destroy
   has_many :tranxactions, as: :tranxactable, dependent: :destroy
 
   # === VALIDATIONS ===
--- app/models/property.rb
+++ app/models/property.rb
@@ -4,7 +4,6 @@ class Property < ApplicationRecord
 	# === ASSOCIATIONS ===
   has_many :tax_categories, through: :tranxactions
   has_many :tenancy_agreements, dependent: :restrict_with_error
-  has_many :tranxactables, as: :resource, dependent: :restrict_with_error
   has_many :tranxactions, as: :tranxactable, dependent: :restrict_with_error
 
   # === VALIDATIONS ===
