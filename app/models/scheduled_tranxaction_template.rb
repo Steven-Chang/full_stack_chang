a5a128449c1d209287b199558b0b7f93bc86e54a
Steven Chang <stevenchang5000@gmail.com>
2019-08-10 11:20:15 +1000

    Setup rake and class method to process scheduled tranxaction templates (#119)

--- app/models/scheduled_tranxaction_template.rb
+++ app/models/scheduled_tranxaction_template.rb
@@ -10,7 +10,24 @@ class ScheduledTranxactionTemplate < ApplicationRecord
 	validates :amount, numericality: true
 	validates :date, :days_for_recurrence, :tranxactable, presence: true
 
-	# === CLASS METHODS ===
-	def self.process_scheduled_tranxaction_templates
-	end
+  # === CLASS METHODS ===
+  def self.process
+    where(enabled: true)
+      .where('date <= ?', Date.current)
+      .find_each do |scheduled_tranxaction_template|
+        tranxaction = Tranxaction.create(
+          amount: scheduled_tranxaction_template.amount,
+          creditor: scheduled_tranxaction_template.creditor,
+          date: scheduled_tranxaction_template.date,
+          description: scheduled_tranxaction_template.description,
+          tax: scheduled_tranxaction_template.tax,
+          tax_category: scheduled_tranxaction_template.tax_category,
+          tranxactable: scheduled_tranxaction_template.tranxactable
+        )
+        next unless tranxaction.persisted?
+
+        next_date_to_process = scheduled_tranxaction_template.date + scheduled_tranxaction_template.days_for_recurrence.days
+        scheduled_tranxaction_template.update!(date: next_date_to_process)
+		  end
+  end
 end
