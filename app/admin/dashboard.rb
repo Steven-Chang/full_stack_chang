8549deb314b8c283a19bf6b13efd1068bbcc8525
Steven Chang <steven@chilfundraising.com>
2019-08-12 13:19:51 +1000

    Change rent to tenancy agreements in aa dashboard; format dashboard code

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -17,6 +17,7 @@ ActiveAdmin.register_page 'Dashboard' do
       column do
         panel '18-19 Gross Income' do
           ul do
+            h3 'Clients'
             Client.all.map do |client|
               li "#{client.name}: $#{client.tranxactions
                                            .where('date >= ?', Date.new(2018, 7, 1))
@@ -25,23 +26,30 @@ ActiveAdmin.register_page 'Dashboard' do
                                            .where(tax: true)
                                            .sum(:amount)}"
             end
-            li "Rent: $#{Tranxaction.where('date >= ?', Date.new(2018, 7, 1))
+            h3 'TenancyAgreement'
+            li "TenancyAgreement: $#{Tranxaction.where('date >= ?', Date.new(2018, 7, 1))
                                     .where('date < ?', Date.new(2019, 7, 1))
                                     .where('amount > 0')
                                     .where(tranxactable_type: 'TenancyAgreement')
                                     .where(tax: true)
                                     .sum(:amount)}"
+            h3 'Property'
             li "Property: $#{Tranxaction.where('date >= ?', Date.new(2018, 7, 1))
                                         .where('date < ?', Date.new(2019, 7, 1))
                                         .where('amount > 0')
                                         .where(tranxactable_type: 'Property')
                                         .where(tax: true)
                                         .sum(:amount)}"
+
+            hr
+
+            h3 'Incoming tranxactions that are not Clients, TenancyAgreement or Property'
             Tranxaction.where('date >= ?', Date.new(2018, 7, 1))
                        .where('date < ?', Date.new(2019, 7, 1))
                        .where('amount > 0')
                        .where(tax: true)
                        .where.not(tranxactable_type: 'Client')
+                       .where.not(tranxactable_type: 'Property')
                        .where.not(tranxactable_type: 'TenancyAgreement')
                        .map do |tranxaction|
               li "#{tranxaction.description}: $#{tranxaction.amount}"
@@ -52,26 +60,35 @@ ActiveAdmin.register_page 'Dashboard' do
 
       column do
         panel '18-19 Expenses' do
+          h3 'Clients'
           Client.all.map do |client|
-            li "#{client.name}: $#{client.tranxactions
-                                         .where('date >= ?', Date.new(2018, 7, 1))
-                                         .where('date < ?', Date.new(2019, 7, 1))
-                                         .where('amount < 0')
-                                         .where(tax: true)
-                                         .sum(:amount)}"
+            li "#{client.name}: #{number_to_currency(client.tranxactions
+                                                           .where('date >= ?', Date.new(2018, 7, 1))
+                                                           .where('date < ?', Date.new(2019, 7, 1))
+                                                           .where('amount < 0')
+                                                           .where(tax: true)
+                                                           .sum(:amount))}"
           end
-          li "Rent: $#{Tranxaction.where('date >= ?', Date.new(2018, 7, 1))
-                                  .where('date < ?', Date.new(2019, 7, 1))
-                                  .where('amount < 0')
-                                  .where(tranxactable_type: 'TenancyAgreement')
-                                  .where(tax: true)
-                                  .sum(:amount)}"
-          li "Property: $#{Tranxaction.where('date >= ?', Date.new(2018, 7, 1))
-                                      .where('date < ?', Date.new(2019, 7, 1))
-                                      .where('amount < 0')
-                                      .where(tranxactable_type: 'Property')
-                                      .where(tax: true)
-                                      .sum(:amount)}"
+
+          h3 'TenancyAgreement'
+          li number_to_currency(Tranxaction.where('date >= ?', Date.new(2018, 7, 1))
+                                           .where('date < ?', Date.new(2019, 7, 1))
+                                           .where('amount < 0')
+                                           .where(tranxactable_type: 'TenancyAgreement')
+                                           .where(tax: true)
+                                           .sum(:amount))
+
+          h3 'Property'
+          li number_to_currency(Tranxaction.where('date >= ?', Date.new(2018, 7, 1))
+                                          .where('date < ?', Date.new(2019, 7, 1))
+                                          .where('amount < 0')
+                                          .where(tranxactable_type: 'Property')
+                                          .where(tax: true)
+                                          .sum(:amount))
+
+          hr
+
+          h3 'Expense tranxactions that are not Clients, TenancyAgreement or Property'
           Tranxaction.where('date >= ?', Date.new(2018, 7, 1))
                      .where('date < ?', Date.new(2019, 7, 1))
                      .where('amount < 0')
@@ -80,7 +97,7 @@ ActiveAdmin.register_page 'Dashboard' do
                      .where.not(tranxactable_type: 'TenancyAgreement')
                      .where.not(tranxactable_type: 'Property')
                      .map do |tranxaction|
-            li "#{tranxaction.description}: $#{tranxaction.amount}"
+            li "#{tranxaction.description}: #{number_to_currency(tranxaction.amount)}"
           end
         end
       end
