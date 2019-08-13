9a0a0c22b6b55ab75f6246efb814396f2a84ee40
Steven Chang <steven@chilfundraising.com>
2019-08-13 20:37:46 +1000

    Move projects to ActiveAdmin and do a general clean up

---
+++ app/admin/projects.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# params['commit'] exists when filters are selected
+ActiveAdmin.register Project do
+  # === INDEX ===
+  index do
+    column :title
+    column :url do |project|
+      link_to 'link', project.url
+    end
+    column :start_date
+    column :end_date
+    column :private
+    column :attachments do |tranxaction|
+      next if tranxaction.attachments.blank?
+
+      table_for tranxaction.attachments.order('created_at DESC') do
+        column 'Attachments' do |attachment|
+          link_to attachment.url, attachment.url, target: '_blank', rel: 'noopener'
+        end
+      end
+    end
+    actions
+  end
+
+  # === SHOW ===
+  show do
+    attributes_table do
+      row :id
+      row :title
+      row :description
+      row :url do |project|
+        link_to 'link', project.url
+      end
+      row :start_date
+      row :end_date
+      row :private
+      table_for project.attachments.order('created_at DESC') do
+        column 'Attachments' do |attachment|
+          link_to attachment.url, attachment.url, target: '_blank', rel: 'noopener'
+        end
+      end
+    end
+  end
+end
--- app/serializers/tranxaction_serializer.rb
+++
@@ -1,6 +0,0 @@
-class TranxactionSerializer < ActiveModel::Serializer
-  attributes :id, :date, :description, :amount, :tax, :tax_category_id
-
-  belongs_to :tax_category
-  has_many :attachments
-end
