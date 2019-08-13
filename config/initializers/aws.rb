842edafb63c2278b474a11511c1d0bea04bbacbd
Steven Chang <steven@chilfundraising.com>
2019-08-13 23:20:03 +1000

    Display attachments as urls for tranxactions

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,9 @@
-Aws.config.update({
+# frozen_string_literal: true
+
+Aws.config.update(
   region: 'ap-southeast-2',
   credentials: Aws::Credentials.new(
     Rails.application.credentials.aws[:AWS_ACCESS_KEY_ID],
     Rails.application.credentials.aws[:AWS_SECRET_ACCESS_KEY]
-})
+  )
+)
