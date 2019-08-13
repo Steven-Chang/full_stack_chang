4441d088bf094db34ea76aa1a9c70a3274fceeea
Steven Chang <steven@chilfundraising.com>
2019-08-13 23:13:28 +1000

    AwS use new encrypted keys syntax

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 Aws.config.update({
-    region: 'ap-southeast-2',
-    credentials: Aws::Credentials.new(
-      ENV['AWS_ACCESS_KEY_ID'], 
-      ENV['AWS_SECRET_ACCESS_KEY'])
+  region: 'ap-southeast-2',
+  credentials: Aws::Credentials.new(
+    Rails.application.credentials.aws[:AWS_ACCESS_KEY_ID],
+    Rails.application.credentials.aws[:AWS_SECRET_ACCESS_KEY]
 })
