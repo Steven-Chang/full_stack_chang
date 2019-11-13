64415bf69dd5c7ee0d9c8829ff0951ef4a9c5737
Steven Chang <steven@chilfundraising.com>
2019-11-13 19:03:53 +1100

    ActiveAdmin BlogPosts Index: whitespace pre-wrap
    ActiveAdmin BlogPot Index: Image height 150px

--- app/admin/blog_posts.rb
+++ app/admin/blog_posts.rb
@@ -12,12 +12,16 @@ ActiveAdmin.register BlogPost do
       span blog_post.title, class: 'title'
     end
     body do |blog_post|
-      div blog_post.date_added.strftime('%d %B %Y'), class: 'date_added'
-      pre blog_post.description
-      blog_post.attachments.each do |attachment|
-        if attachment.cloudinary_public_id.present?
-          div(style: 'border: 1px solid lightgray; display: inline-block;') do
-            cl_image_tag(attachment.cloudinary_public_id, height: 120, fetch_format: :auto, crop: :crop, width: 120, style: 'width: inherit;')
+      div class: 'container' do
+        div blog_post.date_added.strftime('%d %B %Y'), class: 'date_added'
+        div(style: "white-space: pre-wrap;") do
+          blog_post.description
+        end
+        blog_post.attachments.each do |attachment|
+          if attachment.cloudinary_public_id.present?
+            div(style: 'border: 1px solid lightgray; display: inline-block; height: 150px;') do
+              cl_image_tag(attachment.cloudinary_public_id, height: 240, fetch_format: :auto, style: 'width: inherit; height: 100%;')
+            end
           end
         end
       end
