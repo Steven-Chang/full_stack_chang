14f94388b2678a151d9cc04cd17ee9bb185c3493
Steven Chang <stevenchang5000@gmail.com>
2020-02-26 13:27:38 +1100

    Remove entries from routes and entries controller (#173)

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@
 Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'entries/by_date' => 'entries#by_date'
-  resources :entries, only: %i[update destroy]
   resources :scores, only: %i[index create]
   root 'application#home'
 end
