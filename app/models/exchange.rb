880ee8354ed821d39da3c3c7851cfe2747bb93da
Steven Chang <stevenchang5000@gmail.com>
2020-09-03 23:57:36 +1000

    Add Exchange associations and validations (#191)
    Co-authored-by: Steven Chang <steven@chilfundraising.com>

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Exchange < ApplicationRecord
+  # === ASSOCIATIONS ===
+  has_many :crypto_exchanges, dependent: :destroy
+  has_many :cryptos, through: :crypto_exchanges
+
+  # === VALIDATIONS ===
+  validates :identifier, :name, presence: true
 end
--- spec/models/exchange_spec.rb
+++ spec/models/exchange_spec.rb
@@ -3,5 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe Exchange, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ASSOCIATIONS' do
+    it { should have_many(:crypto_exchanges).dependent(:destroy) }
+    it { should have_many(:cryptos) }
+  end
+
+  describe 'VALIDATIONS' do
+    it { should validate_presence_of(:identifier) }
+    it { should validate_presence_of(:name) }
+  end
 end
