55ff7eaf312d2bc47e00a48602fdfccf1b1e3752
Steven Chang <stevenchang5000@gmail.com>
2020-09-03 23:46:34 +1000

    Add Crypto presence validations (#190)
    Co-authored-by: Steven Chang <steven@chilfundraising.com>

--- spec/models/crypto_spec.rb
+++ spec/models/crypto_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Crypto, type: :model do
     it { should have_many(:crypto_exchanges).dependent(:destroy) }
     it { should have_many(:exchanges) }
   end
+
+  describe 'VALIDATIONS' do
+    it { should validate_presence_of(:identifier) }
+    it { should validate_presence_of(:name) }
+  end
 end
