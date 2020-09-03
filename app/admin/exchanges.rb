a3633d83670ef8d1270519c5c7ed418567d6e73b
Steven Chang <steven@chilfundraising.com>
2020-09-03 23:44:44 +1000

    Add crypto exchanges to active admin

---
+++ app/admin/exchanges.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+# params['commit'] exists when filters are selected
+ActiveAdmin.register Exchange do
+  # === CONFIG ===
+  config.sort_order = 'identifier_asc'
+
+  # === INDEX ===
+  index do
+    column :identifier
+    column :name
+    column :maker_fee
+    column :taker_fee
+    column :fiat_withdrawal_fee
+    actions
+  end
+
+  filter :identifier
+  filter :name
+
+  # === SHOW ===
+  show do
+    attributes_table do
+      row :identifier
+      row :name
+      row :url
+      row :maker_fee
+      row :taker_fee
+      row :fiat_withdrawal_fee
+    end
+  end
+
+  # === FORM ===
+  form do |f|
+    f.inputs do
+      f.input :identifier
+      f.input :name
+      f.input :url
+      f.input :maker_fee
+      f.input :taker_fee
+      f.input :fiat_withdrawal_fee
+    end
+    f.actions
+  end
+
+  # === PERMIT PARAMS ===
+  permit_params :identifier, :fiat_withdrawal_fee, :maker_fee, :name, :taker_fee, :url
+end
