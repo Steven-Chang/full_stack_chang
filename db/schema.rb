a71b863ec109191e13d6e2948001703ebfe66190
Steven Chang <steven@chilfundraising.com>
2020-09-04 00:03:07 +1000

    Add foreign key relationship on crypto exchanges and exchange id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_132104) do
+ActiveRecord::Schema.define(version: 2020_09_03_140217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -237,6 +237,7 @@ ActiveRecord::Schema.define(version: 2020_09_03_132104) do
 
   add_foreign_key "achievements", "projects", name: "achievements_project_id_fk"
   add_foreign_key "crypto_exchanges", "cryptos", name: "crypto_exchanges_crypto_id_fk"
+  add_foreign_key "crypto_exchanges", "exchanges", name: "crypto_exchanges_exchange_id_fk"
   add_foreign_key "payment_summaries", "clients", name: "payment_summaries_client_id_fk"
   add_foreign_key "projects_tools", "projects", name: "projects_tools_project_id_fk"
   add_foreign_key "projects_tools", "tools", name: "projects_tools_tool_id_fk"
