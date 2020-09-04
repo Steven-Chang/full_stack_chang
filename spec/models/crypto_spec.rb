e9daa362ffb1c4bc50a5ef78642fb0efc300a868
Steven Chang <stevenchang5000@gmail.com>
2020-09-05 07:55:50 +1000

    Crypto: identifier to be downcased (#194)
    Co-authored-by: Steven Chang <steven@chilfundraising.com>

--- spec/models/crypto_spec.rb
+++ spec/models/crypto_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe Crypto, type: :model do
     it { should validate_presence_of(:identifier) }
     it { should validate_presence_of(:name) }
   end
+
+  describe 'CALLBACKS' do
+    describe 'before_save' do
+      it 'downcases the identifier' do
+        crypto = build(:crypto, identifier: 'BTC')
+        crypto.save
+        expect(crypto.identifier).to eq('btc')
+      end
+    end
+  end
 end
