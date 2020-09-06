7aace80974654a76cb97bd4d2aedb1e5a972ae22
Steven Chang <steven@chilfundraising.com>
2020-09-06 11:54:52 +1000

    Exchange.create_default_exchanges

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,19 +1,44 @@
 # frozen_string_literal: true
 
 class Exchange < ApplicationRecord
+  DEFAULT_EXCHANGES = {
+    coinspot: {
+      name: 'CoinSpot',
+      url: 'https://www.coinspot.com.au/',
+      maker_fee: 0.1,
+      taker_fee: 0.1
+    },
+    binance: {
+      name: 'Binance',
+      url: 'https://www.binance.com/en',
+      maker_fee: 0.1,
+      taker_fee: 0.1
+    }
+  }.freeze
+
   # === ASSOCIATIONS ===
   has_many :trade_pairs, dependent: :destroy
 
   # === VALIDATIONS ===
   validates :identifier, :name, presence: true
 
-  def get_open_orders(crypto_identifier, buy_or_sell, number_of_orders = 1)
-    crypto = Crypto.find_by(identifier: crypto_identifier.downcase)
+  def self.arbitrage_opportunity_available
 
+  end
+
+  def self.create_default_exchanges
+    DEFAULT_EXCHANGES.each do |identifier, value|
+      next if Exchange.find_by(identifier: identifier)
+
+      Exchange.create(identifier: identifier, name: value[:name], url: value[:url], maker_fee: value[:maker_fee], taker_fee: value[:taker_fee])
+    end
+  end
+
+  def get_open_orders(symbol, buy_or_sell, number_of_orders = 1)
     case identifier
     when 'coinspot'
       m = Mechanize.new
-      url = crypto_exchanges.find_by(crypto: crypto).url
+      url = trade_pairs.find_by(symbol: symbol).url
       page = m.get(url)
       selector = buy_or_sell == 'buy' ? '.openbuyrows' : '.opensellrows'
       orders = []
--- spec/models/exchange_spec.rb
+++ spec/models/exchange_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Exchange, type: :model do
+  let(:default_exchange) { create }
+
   describe 'ASSOCIATIONS' do
     it { should have_many(:trade_pairs).dependent(:destroy) }
   end
@@ -10,5 +12,32 @@ RSpec.describe Exchange, type: :model do
   describe 'VALIDATIONS' do
     it { should validate_presence_of(:identifier) }
     it { should validate_presence_of(:name) }
+  end
+
+  describe 'CLASS METHODS' do
+    describe '.create_default_exchanges' do
+      context 'when default exchange already exists' do
+        before { create(:exchange, identifier: Exchange::DEFAULT_EXCHANGES.keys.first) }
+
+        it 'does not re-create the default_exchange' do
+          expect(Exchange.where(identifier: 'coinspot').count).to be 1
+          Exchange.create_default_exchanges
+          expect(Exchange.where(identifier: 'coinspot').count).to be 1
+        end
+      end
+
+      context 'when default exchange does not exist' do
+        it 'creates the default exchanges' do
+          expect(Exchange.count).to be 0
+          Exchange.create_default_exchanges
+          expect(Exchange.count).to be Exchange::DEFAULT_EXCHANGES.keys.count
+          expect(Exchange.first.identifier).to eq(Exchange::DEFAULT_EXCHANGES.first.first.to_s)
+          expect(Exchange.first.name).to eq(Exchange::DEFAULT_EXCHANGES.first.second[:name])
+          expect(Exchange.first.url).to eq(Exchange::DEFAULT_EXCHANGES.first.second[:url])
+          expect(Exchange.first.maker_fee).to eq(Exchange::DEFAULT_EXCHANGES.first.second[:maker_fee])
+          expect(Exchange.first.taker_fee).to eq(Exchange::DEFAULT_EXCHANGES.first.second[:taker_fee])
+        end
+      end
+    end
   end
 end
