541f44e942c79202d5b8cc76dd5a35e0155ba02c
Steven Chang <steven@chilfundraising.com>
2020-09-06 10:59:47 +1000

    Property associations fix

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,9 +2,9 @@
 
 class Property < ApplicationRecord
 	# === ASSOCIATIONS ===
+  has_many :tranxactions, as: :tranxactable, dependent: :restrict_with_error
   has_many :tax_categories, through: :tranxactions
   has_many :tenancy_agreements, dependent: :restrict_with_error
-  has_many :tranxactions, as: :tranxactable, dependent: :restrict_with_error
 
   # === VALIDATIONS ===
   validates :address, presence: true
