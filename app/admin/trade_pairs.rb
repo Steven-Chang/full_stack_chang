13b72a8461298ff4e73fde4efb763f75682b34de
Steven Chang <stevenchang5000@gmail.com>
2020-09-08 07:54:48 +1000

    Add trade pair to active admin (#208)
    * Trade pair: Add total min and steps
    
    * add-active-to-trade-pairs
    
    * Remove simple-cov
    
    * Update seed file
    
    * Dashboard update to handle no data
    
    * Add trade pair to active admin
    
    * Remove simple cov from spec help file
    
    * Project factory fix
    
    Co-authored-by: Steven Chang <steven@chilfundraising.com>

---
+++ app/admin/trade_pairs.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+# params['commit'] exists when filters are selected
+ActiveAdmin.register TradePair do
+  # === CONFIG ===
+  config.sort_order = 'symbol_asc'
+
+  # === INDEX ===
+  index do
+    column :symbol
+    column :exchange do |trade_pair|
+      trade_pair.exchange.identifier
+    end
+    column :maker_fee
+    column :taker_fee
+    column :minimum_total
+    column :amount_step
+    column :active_for_accumulation
+    actions
+  end
+
+  filter :symbol
+  filter :exchange
+
+  # === SHOW ===
+  show do
+    attributes_table do
+      row :symbol
+      row :exchange do |trade_pair|
+        trade_pair.exchange.identifier
+      end
+      row :url
+      row :maker_fee
+      row :taker_fee
+      row :minimum_total
+      row :amount_step
+      row :active_for_accumulation
+    end
+  end
+
+  # === FORM ===
+  form do |f|
+    f.inputs do
+      f.input :symbol
+      f.input :exchange do |trade_pair|
+        trade_pair.exchange.identifier
+      end
+      f.input :url
+      f.input :maker_fee
+      f.input :taker_fee
+      f.input :minimum_total
+      f.input :amount_step
+      f.input :active_for_accumulation
+    end
+    f.actions
+  end
+
+  # === PERMIT PARAMS ===
+  permit_params :symbol, :exchange, :url, :maker_fee, :taker_fee, :minimum_total, :amount_step, :active_for_accumulation
+end
