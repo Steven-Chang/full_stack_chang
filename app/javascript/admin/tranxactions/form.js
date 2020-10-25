b3fece869c1fc93b406d3b1983bfe0716ad99368
Steven Chang <stevenchang5000@gmail.com>
2020-10-25 17:11:15 +1000

    Move cloudinary upload widget javascript to own file (#254)
    Co-authored-by: Steven Chang <steven@chilfundraising.com>

--- app/javascript/admin/tranxactions/form.js
+++ app/javascript/admin/tranxactions/form.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  if($(".formtastic.tranxaction").length) {
+  if($("form.tranxaction").length) {
     $('#tranxaction_tranxactable_type').change(function() {
       if($('#tranxaction_tranxactable_type').val().length) {
         $('#tranxaction_tranxactable_id_input').show();
@@ -7,23 +7,5 @@ $(document).ready(function() {
         $('#tranxaction_tranxactable_id_input').hide();
       };
     });
-  };
-
-  if($("#edit_tranxaction").length || $("#new_tranxaction").length) {
-    // Cloudinary - Upload
-    var myWidget = cloudinary.createUploadWidget({
-      cloudName: "hpxlnqput",
-      uploadPreset: "jumnv4bk"}, (error, result) => {
-      if (!error && result && result.event === "success") {
-        console.log("Done! Here is the image info: ", result.info);
-        $(".has_many_add").first().click();
-        $(".has_many_fields").last().find("input").first().val(result.info.public_id);
-        $(".has_many_fields").last().find("select").first().val(result.info.resource_type);
-      }
-    })
-    document.getElementById("upload_widget").addEventListener("click", function(evt){
-      myWidget.open();
-      evt.preventDefault();
-    }, false);
   };
 });
