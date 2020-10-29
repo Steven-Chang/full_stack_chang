3d3fb18a1cba6651a188d4f121fa6780a8b49bfe
Steven Chang <steven@chilfundraising.com>
2020-10-29 22:56:55 +1100

    Twilio sms example script

---
+++ scripts/twilio_sms_example.rb
@@ -0,0 +1,11 @@
+@client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+
+@client.region = 'au1'
+
+@client.messages.create(
+  from: '+12054152599',
+  to: '+61405818525',
+  body: 'Hey there!'
+)
+
+# Sent from your Twilio trial account - Hey there!
