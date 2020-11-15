e087d54a0722b048fb134bb255ee0b668da750e2
Steven Chang <steven@chilfundraising.com>
2020-11-15 21:51:38 +1100

    Fix new attachment not triggering new attachment button

--- app/javascript/admin/cloudinary_upload_widget.js
+++ app/javascript/admin/cloudinary_upload_widget.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         uploadPreset: "jumnv4bk"}, (error, result) => {
         if (!error && result && result.event === "success") {
           console.log("Done! Here is the image info: ", result.info);
-          $(".has_many_add").first().click();
+          $(".has_many_add")[0].click();
           $(".has_many_fields").last().find("input").first().val(result.info.public_id);
           if(value == "form.tranxaction") {
             $(".has_many_fields").last().find("select").first().val(result.info.resource_type);
