268da84400f0150bdfbcdae4818108db3f6567a4
Steven Chang <stevenchang5000@gmail.com>
2020-11-15 21:55:50 +1100

    Fix attachments error bug (#264)
    * Fix new attachment not triggering new attachment button
    
    * New Attachment resource_type selection fix
    
    Co-authored-by: Steven Chang <steven@chilfundraising.com>

--- app/javascript/admin/cloudinary_upload_widget.js
+++ app/javascript/admin/cloudinary_upload_widget.js
@@ -8,9 +8,9 @@ $(document).ready(function() {
         uploadPreset: "jumnv4bk"}, (error, result) => {
         if (!error && result && result.event === "success") {
           console.log("Done! Here is the image info: ", result.info);
-          $(".has_many_add").first().click();
+          $(".has_many_add")[0].click();
           $(".has_many_fields").last().find("input").first().val(result.info.public_id);
-          if(value == "form.tranxaction") {
+          if(value == "form.tranxaction" || value == "form.blog_post") {
             $(".has_many_fields").last().find("select").first().val(result.info.resource_type);
           };
         }
