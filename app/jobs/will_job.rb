efe8eb6fb224025f124ba9e1102c6df0fd711f50
Steven Chang <steven@chilfundraising.com>
2020-11-27 21:14:33 +1100

    WillJob: Update beneficiaries to include Emily & Chris

--- app/jobs/will_job.rb
+++ app/jobs/will_job.rb
@@ -8,49 +8,46 @@ class WillJob < ApplicationJob
   end
 
   def perform
-    # beneficiaries = {
-    #   'Emily Chen': {
-    #     email: 'emilychentszyan@gmail.com',
-    #     phone_number: '0452 623 866'
-    #   },
-    #   'Chris Asnins': {
-    #     email: 'asnins@live.com.au',
-    #     phone_number: '0450 677 138'
-    #   }
-    # }
-
     beneficiaries = {
+      'Emily Chen': {
+        email: 'emilychentszyan@gmail.com',
+        phone_number: '0452 623 866'
+      },
+      'Chris Asnins': {
+        email: 'asnins@live.com.au',
+        phone_number: '0450 677 138'
+      },
       'Steven Chang': {
         email: 'stevenchang5000@gmail.com',
         phone_number: '0405 818 525'
       }
     }
 
-    # if Rails.env.production? && BlogPost.where('created_at > ?', Time.current - 30.days).empty?
-    if Rails.env.production?
-      beneficiaries.each do |_key, value|
-        # beneficiary_name = key
-        beneficiary_email = value[:email]
-        # beneficiary_phone_number = value[:phone_number]
+    return unless Rails.env.production?
+    return if BlogPost.where('created_at > ?', Time.current - 30.days).present?
 
-        mail = SendGrid::Mail.new
-        mail.from = Email.new(email: 'stevenchang5000@gmail.com', name: 'Steven Chang')
-        personalization = Personalization.new
-        personalization.add_to(Email.new(email: beneficiary_email))
-        # personalization.add_dynamic_template_data(dynamic_template_data)
-        mail.add_personalization(personalization)
-        mail.template_id = 'd-84b29bcd9de54ca7aaa6446e7cf646ad'
+    beneficiaries.each do |_key, value|
+      # beneficiary_name = key
+      beneficiary_email = value[:email]
+      # beneficiary_phone_number = value[:phone_number]
 
-        sg = SendGrid::API.new(api_key: Rails.application.credentials.sendgrid[:api_key])
-        begin
-          response = sg.client.mail._('send').post(request_body: mail.to_json)
-        rescue StandardError => e
-          Rails.logger.debug e.message
-        end
-        Rails.logger.debug response.status_code
-        Rails.logger.debug response.body
-        Rails.logger.debug response.headers
+      mail = SendGrid::Mail.new
+      mail.from = Email.new(email: 'stevenchang5000@gmail.com', name: 'Steven Chang')
+      personalization = Personalization.new
+      personalization.add_to(Email.new(email: beneficiary_email))
+      # personalization.add_dynamic_template_data(dynamic_template_data)
+      mail.add_personalization(personalization)
+      mail.template_id = 'd-84b29bcd9de54ca7aaa6446e7cf646ad'
+
+      sg = SendGrid::API.new(api_key: Rails.application.credentials.sendgrid[:api_key])
+      begin
+        response = sg.client.mail._('send').post(request_body: mail.to_json)
+      rescue StandardError => e
+        Rails.logger.debug e.message
       end
+      Rails.logger.debug response.status_code
+      Rails.logger.debug response.body
+      Rails.logger.debug response.headers
     end
   end
 end
