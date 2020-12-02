8eb55c08f49f04b06aaa05c53920b939946b0fdc
Steven Chang <steven@chilfundraising.com>
2020-12-03 01:35:34 +1100

    ProcessOpenOrderJob update

--- app/jobs/process_open_order_job.rb
+++ app/jobs/process_open_order_job.rb
@@ -3,7 +3,7 @@
 class ProcessOpenOrderJob < ApplicationJob
   def perform(order_id)
     if Order.where('updated_at > ?', Time.current - 1.minute).count > 777
-      ProcessOpenOrderJob.set(wait_until: Time.zone.now + 3.minutes).perform_later(order_id)
+      ProcessOpenOrderJob.set(wait_until: Time.zone.now + 59.seconds).perform_later(order_id)
     elsif (order = Order.find_by(id: order_id))
       order.update_from_exchange
       return unless order.persisted?
