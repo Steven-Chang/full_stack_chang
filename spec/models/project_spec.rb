30bbf9105d22bbd3cf65025ff5758b830ff43c2f
Steven Chang <steven@chilfundraising.com>
2020-12-02 14:31:38 +1100

    Project: Remove date_formatted

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -16,41 +16,4 @@ RSpec.describe Project, type: :model do
     it { should validate_presence_of(:start_date) }
     it { should validate_presence_of(:title) }
   end
-
-  describe 'INSTANCE METHODS' do
-    describe '#duration_formatted' do
-      context 'when project has no start_date' do
-        it 'returns nil' do
-          project.start_date = nil
-          expect(project.duration_formatted).to be nil
-        end
-      end
-
-      context 'when project has a start_date' do
-        context 'when project does not have an end date' do
-          it 'sets the duration using the current date' do
-            project.end_date = nil
-            project.start_date = Date.current - 1.day
-            expect(project.duration_formatted).to eq('1 day')
-            project.start_date = Date.current - 31.days
-            expect(project.duration_formatted).to eq('1 month 1 day')
-            project.start_date = Date.current - 792.days
-            expect(project.duration_formatted).to eq('2 years 2 months 2 days')
-          end
-        end
-
-        context 'when project has an end date' do
-          it 'sets the duration using the end date' do
-            project.end_date = Date.current - 1.day
-            project.start_date = project.end_date - 1.day
-            expect(project.duration_formatted).to eq('1 day')
-            project.start_date = project.end_date - 31.days
-            expect(project.duration_formatted).to eq('1 month 1 day')
-            project.start_date = project.end_date - 427.days
-            expect(project.duration_formatted).to eq('1 year 2 months 2 days')
-          end
-        end
-      end
-    end
-  end
 end
