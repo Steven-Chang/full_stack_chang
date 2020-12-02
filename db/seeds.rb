f7acf6da6de538a98bd8e97e53631d0f4f799e24
Steven Chang <steven@chilfundraising.com>
2020-12-02 21:53:59 +1100

    Remove tetris reference in Seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,6 @@ if Rails.env.development? && User.find_by(email: 'admin@example.com').nil?
   User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 end
 
-# === PROJECTS ===
-if Project.find_by(title: 'tetris').nil?
-  puts 'Creating project Tetris'
-  Project.create!(
-    title: 'tetris',
-    private: false,
-    start_date: Date.current
-  )
-
-  puts 'Creating scores for Tetris'
-  NUMBER_OF_SCORES = 5
-  NUMBER_OF_SCORES.times do |n|
-    Project.find_by(title: 'tetris')
-           .scores
-           .create(
-             level: n,
-             lines: n,
-             score: n,
-             name: 'stevenchang50000'
-           )
-  end
-end
-
 # === EXCHANGES ===
 puts 'Creating exchanges'
 Exchange.create_default_exchanges
