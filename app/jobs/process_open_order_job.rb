7adc68970f99eece5752cab1c1d4f8b71223c3e9
Steven Chang <steven@chilfundraising.com>
2020-12-05 14:28:07 +1100

    ProcessOpenOrderJob: Skip if order was updaed in the last minute

--- app/jobs/process_open_order_job.rb
+++ app/jobs/process_open_order_job.rb
@@ -3,6 +3,7 @@
 class ProcessOpenOrderJob < ApplicationJob
   def perform(order_id)
     return unless (order = Order.find_by(id: order_id))
+    return if order.updated_at > Time.current - 1.minute
     return if order.credential.orders.where('orders.updated_at > ?', Time.current - 1.minute).count > 333
 
     order.update_from_exchange
