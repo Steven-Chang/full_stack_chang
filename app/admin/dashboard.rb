7ea06c4608b250a77839a5c61de3221ae12d0cb5
Steven Chang <steven@chilfundraising.com>
2020-12-06 10:51:58 +1100

    ActiveAdmin: Average minutes to scalp change to minutes

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,8 +4,7 @@ ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
   content title: proc { I18n.t('active_admin.dashboard') } do
-    h4 'Accumulate'
-    h6 'Decrease time between sales is the aim'
+    h4 'Average minutes scalp'
     div do
       line_chart minutes_to_sale_path
     end
