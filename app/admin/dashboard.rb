1df5d463463ccd318fdbce94c36ab6fe7b1e53b0
Steven Chang <steven@chilfundraising.com>
2020-12-08 13:47:24 +1100

    ActiveAdmin: Add bootstrap columns to the dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,16 +4,6 @@ ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
   content title: proc { I18n.t('active_admin.dashboard') } do
-    panel 'Average minutes to scalp' do
-      line_chart minutes_to_sale_path, xtitle: 'Date scalp order created', ytitle: 'Minutes to fill scalp order'
-    end
-
-    panel 'Total scalped' do
-      line_chart total_scalped_path, xtitle: 'Date scalp order created', ytitle: '# of scalp orders'
-    end
-
-    panel 'Cancelled stale orders vs percentage from market price' do
-      bar_chart Order.where(status: 'cancelled_stale').where.not(percentage_from_market_price: nil).group(:percentage_from_market_price).count
-    end
+    render partial: 'active_admin/dashboard/crypto_dashboard'
   end
 end
