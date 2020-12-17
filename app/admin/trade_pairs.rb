01ad6cfae96feaf82f91d4d0754905953a0cabd0
Steven Chang <stevenchang5000@gmail.com>
2020-12-18 06:40:40 +1100

    Active admin add set mode to counter (#303)
    * ActiveAdmin trade pairs: enable updating modes via batch actions
    
    * Active admin trade pairs: Batch actions success message updated
    
    Co-authored-by: Steven Chang <steven@chilfundraising.com>

--- app/admin/trade_pairs.rb
+++ app/admin/trade_pairs.rb
@@ -5,6 +5,28 @@ ActiveAdmin.register TradePair do
   # === CONFIG ===
   config.sort_order = 'symbol_asc'
 
+  # === BATCH ACTIONS ===
+  batch_action :mode_buy do |ids|
+    batch_action_collection.find(ids).each do |trade_pair|
+      trade_pair.update!(mode: 'buy')
+    end
+    redirect_to collection_path, alert: 'Modes updated to buy'
+  end
+
+  batch_action :mode_counter_only do |ids|
+    batch_action_collection.find(ids).each do |trade_pair|
+      trade_pair.update!(mode: 'counter_only')
+    end
+    redirect_to collection_path, alert: 'Modes updated to counter_only'
+  end
+
+  batch_action :mode_scalp do |ids|
+    batch_action_collection.find(ids).each do |trade_pair|
+      trade_pair.update!(mode: 0)
+    end
+    redirect_to collection_path, alert: 'Modes updated to scalp'
+  end
+
   # === INDEX ===
   index do
     column :symbol
