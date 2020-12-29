7c5a2442786e41c6caa495b90142991ea364dd83
Steven Chang <steven@chilfundraising.com>
2020-12-29 13:23:23 +1000

    Dashboard to be nested under Crypto

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register_page 'Dashboard' do
-  menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
+  # === MENU ===
+  menu parent: 'Crypto', priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
   content title: proc { I18n.t('active_admin.dashboard') } do
     render partial: 'active_admin/dashboard/crypto_dashboard'
