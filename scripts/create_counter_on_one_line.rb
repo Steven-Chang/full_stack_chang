3a2f98edad484ee8c385185cc42829df27a69af8
Steven Chang <steven@chilfundraising.com>
2021-02-07 10:50:10 +1100

    Enable selling

--- scripts/create_counter_on_one_line.rb
+++ scripts/create_counter_on_one_line.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Credential.find_by(identifier: 'steven').trade_pairs.find_by(symbol: 'btcusdt').orders.where(status: 'filled', buy_or_sell: 'buy').order(price: :asc).find_each do |order|
+Credential.find_by(identifier: 'joe_lam').trade_pairs.find_by(symbol: 'btcusdt').orders.where(status: 'filled', buy_or_sell: 'buy').order(price: :asc).find_each do |order|
   next unless order.quantity_received
 
   order.create_counter
