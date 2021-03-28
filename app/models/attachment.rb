ab906ee59deaf75b0ede03f963a8ac692bfb4f8d
Steven Chang <stevenchang5000@gmail.com>
2021-03-28 11:47:30 +1100

    Add cloudinary_url function to Attachment. Use in home page for proje… (#357)
    * Add cloudinary_url function to Attachment. Use in home page for projects attachment image
    
    * Rubocop fixes

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -4,6 +4,9 @@ class Attachment < ApplicationRecord
   # === ASSOCIATIONS ===
   belongs_to :resource, polymorphic: true
 
+  # === VALIDATES ===
+  validate :url_should_be_blank_when_cloudinary_public_id_present
+
   # === CALLBACKS ===
   after_destroy do |attachment|
     Cloudinary::Uploader.destroy(attachment.cloudinary_public_id) if attachment.cloudinary_public_id
@@ -11,4 +14,19 @@ class Attachment < ApplicationRecord
 
   # === ENUMERABLES ===
   enum file_type: { image: 0, raw: 1, video: 2 }
+
+  # === INSTANCE METHODS ===
+  def cloudinary_url(options = {})
+    return if cloudinary_public_id.blank?
+
+    Cloudinary::Utils.cloudinary_url(cloudinary_public_id, options)
+  end
+
+  private
+
+  def url_should_be_blank_when_cloudinary_public_id_present
+    return if cloudinary_public_id.present? && url.present?
+
+    errors.add :url, :unnecessary, message: 'please use cloudinary_url instead.'
+  end
 end
