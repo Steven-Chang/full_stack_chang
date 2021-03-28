3bf7f3e01609ba18fe651e46b9b8d49397ecd0fb
Steven Chang <stevenchang5000@gmail.com>
2021-03-28 14:37:52 +1100

    Home project attachment to use url

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -4,9 +4,6 @@ class Attachment < ApplicationRecord
   # === ASSOCIATIONS ===
   belongs_to :resource, polymorphic: true
 
-  # === VALIDATES ===
-  validate :url_should_be_blank_when_cloudinary_public_id_present
-
   # === CALLBACKS ===
   after_destroy do |attachment|
     Cloudinary::Uploader.destroy(attachment.cloudinary_public_id) if attachment.cloudinary_public_id
@@ -20,13 +17,5 @@ class Attachment < ApplicationRecord
     return if cloudinary_public_id.blank?
 
     Cloudinary::Utils.cloudinary_url(cloudinary_public_id, options)
-  end
-
-  private
-
-  def url_should_be_blank_when_cloudinary_public_id_present
-    return unless cloudinary_public_id.present? && url.present?
-
-    errors.add :url, :unnecessary, message: 'please use cloudinary_url instead.'
   end
 end
