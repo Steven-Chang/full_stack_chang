4ca51d31bcacd0b3492870c06541cbd5b7d3eca5
Steven Chang <stevenchang5000@gmail.com>
2021-06-04 08:52:40 +1000

    Fix tool attachment missing error on home page

--- scripts/create_counter_on_one_line.rb
+++ scripts/create_counter_on_one_line.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-Credential.find_by(identifier: 'joe_lam').trade_pairs.find_by(symbol: 'btcusdt').orders.where(status: 'filled', buy_or_sell: 'buy').order(price: :asc).find_each do |order|
-  next unless order.quantity_received
+Credential.find_by(identifier: 'steven').trade_pairs.find_by(symbol: 'scrtbtc').orders.where(status: 'filled', buy_or_sell: 'buy').order(price: :asc).where('created_at > ?', Date.current - 3.weeks).where.not(quantity_received: [nil, 0]).find_each do |order|
 
   order.create_counter
 end
