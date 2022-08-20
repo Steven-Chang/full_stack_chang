de2531ffff0bda0d8bb91280aea474f7f908e854
Steven Chang <stevenchang5000@gmail.com>
2022-08-21 06:17:20 +1000

    Refactor ScheduleProcessOpenOrdersJob

--- app/jobs/process_open_order_job.rb
+++ app/jobs/process_open_order_job.rb
@@ -1,22 +1,16 @@
 # frozen_string_literal: true
 
 class ProcessOpenOrderJob < ApplicationJob
+  queue_as :low
+
   def perform(order_id)
     return unless (order = Order.find_by(id: order_id))
-
-    one_minute_ago = 1.minute.ago
-    return if order.updated_at > one_minute_ago
-    return if order.credential.orders.where('orders.updated_at > ?', one_minute_ago).count > 333
 
     order.update_from_exchange
     return unless order.persisted?
 
     trade_pair_mode = order.trade_pair.mode
     order.create_counter if order.filled? && %w[accumulate counter_only].include?(trade_pair_mode)
-    if order.stale?(trade_pair_mode == 'counter_only' ? 0 : 6)
-      order.cancel
-    else
-      order.save(updated_at: Time.current)
-    end
+    order.cancel if order.stale?(trade_pair_mode == 'counter_only' ? 0 : 6)
   end
 end
--- app/jobs/process_open_orders_job.rb
+++ app/jobs/process_open_orders_job.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 class ProcessOpenOrdersJob < ApplicationJob
-  after_perform do |_job|
-    ProcessOpenOrdersJob.set(wait_until: Time.zone.now + 1.minute).perform_later unless ProcessOpenOrdersJob.scheduled?
-  end
+  queue_as :low
 
   def perform
-    Credential.where(enabled: true).find_each do |credential|
-      credential_orders = credential.orders
-      next if credential_orders.where('orders.updated_at > ?', Time.current - 1.minute).count > 333
-
-      credential_orders.where(status: 'open').joins(:trade_pair).where(trade_pairs: { enabled: true }).find_each do |order|
+    Credential.where(enabled: true)
+              .find_each do |credential|
+      credential.orders
+                .where(status: 'open')
+                .joins(:trade_pair)
+                .where(trade_pairs: { enabled: true })
+                .where('orders.created_at < ?', 2.minutes.ago)
+                .find_each do |order|
         ProcessOpenOrderJob.perform_later(order.id)
       end
     end
