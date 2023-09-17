82de259b74099587864f6ad8f544873e9a0fb46f
Steven Chang <stevenchang5000@protonmail.com>
2023-09-17 10:54:09 +1000

    routes: remove scores

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,5 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resources :scores, only: %i[index create]
-  # API
-
   root 'application#home'
 end
