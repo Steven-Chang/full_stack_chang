8667ff9760130012ae2af1406917316316220f9b
Steven Chang <stevenchang5000@protonmail.com>
2023-09-25 10:24:16 +1000

    aa blog posts: attachments update to use active storage (#450)

--- app/admin/blog_posts.rb
+++ app/admin/blog_posts.rb
@@ -20,12 +20,9 @@ ActiveAdmin.register BlogPost do
         div(style: 'white-space: pre-wrap;') do
           blog_post.description
         end
-        blog_post.attachments.where(file_type: 0).limit(9).order('RANDOM()').each do |attachment|
-          next if attachment.cloudinary_public_id.blank?
 
-          div(style: 'border: 1px solid lightgray; display: inline-block; height: 150px;') do
-            cl_image_tag(attachment.cloudinary_public_id, height: 240, fetch_format: :auto, style: 'width: inherit; height: 100%;')
-          end
+        blog_post.attachments do |attachment|
+          link_to attachment.filename.to_s, url_for(attachment), target: '_blank', rel: 'noopener'
         end
       end
     end
@@ -45,16 +42,10 @@ ActiveAdmin.register BlogPost do
       row :date_added
       row :description
       row :private
-      row 'Images' do |blog_post|
-        html = ''
-        blog_post.attachments.where(file_type: 0).each do |attachment|
-          if attachment.url.present?
-            html += "<img src=#{attachment.url}>"
-          elsif attachment.cloudinary_public_id.present?
-            html += cl_image_tag(attachment.cloudinary_public_id, fetch_format: :auto, height: 360, style: 'width: inherit;')
-          end
+      table_for blog_post.attachments.order('created_at DESC') do
+        column 'Attachments' do |attachment|
+          link_to attachment.filename.to_s, url_for(attachment), target: '_blank', rel: 'noopener'
         end
-        html.html_safe
       end
     end
   end
@@ -67,17 +58,10 @@ ActiveAdmin.register BlogPost do
       f.input :date_added, start_year: 1985
       f.input :description
       f.input :private
-      f.has_many :attachments,
-                 heading: 'Attachments',
-                 new_record: 'Manually create an attachment',
-                 allow_destroy: true do |a|
-        a.input :cloudinary_public_id, hint: a.object.file_type == 'image' && a.object.cloudinary_public_id.present? ? cl_image_tag(a.object.cloudinary_public_id, height: 240, fetch_format: :auto, style: 'width: inherit;') : nil
-        a.input :url
-        a.object.file_type = a.object.persisted? ? a.object.file_type : 'video'
-        a.input :file_type, as: :select,
-                            collection: Attachment.file_types.keys,
-                            include_blank: false
+      f.object.attachments.each do |attachment|
+        f.input :attachments, input_html: { multiple: true, value: attachment.signed_id }, as: :hidden
       end
+      f.input :attachments, as: :file, input_html: { multiple: true }
       li '<label>Cloudinary upload</label><button id="upload_widget" class="cloudinary-button">Upload image</button>'.html_safe
     end
     f.actions
@@ -88,5 +72,5 @@ ActiveAdmin.register BlogPost do
                 :description,
                 :private,
                 :title,
-                attachments_attributes: %i[id cloudinary_public_id file_type url _destroy]
+                attachments: []
 end
