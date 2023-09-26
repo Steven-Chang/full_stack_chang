38e64d7c094e2b136045124bb2234b01b0140aae
Steven Chang <stevenchang5000@protonmail.com>
2023-09-26 15:42:35 +1000

    Deprecate crypto dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,10 +1,29 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register_page 'Dashboard' do
-  # === MENU ===
-  menu parent: 'Crypto', priority: 1, label: proc { I18n.t('active_admin.dashboard') }
-
-  content title: proc { I18n.t('active_admin.dashboard') } do
-    render partial: 'active_admin/dashboard/crypto_dashboard'
+  content title: 'Tax Summaries' do
+    h5 '*Other tranxactions: No direct association with Client, Property or TenancyAgreement'
+    Tranxaction.end_of_financial_year_dates_ordered_descending_for_dashboard&.slice(0, 1)&.each do |end_of_financial_year_date|
+      panel "#{end_of_financial_year_date.year - 1}-#{end_of_financial_year_date.year}" do
+        render partial: 'active_admin/dashboard/summary',
+               locals: {
+                 end_of_financial_year_date:
+               }
+        render partial: 'active_admin/dashboard/tranxactions',
+               locals: {
+                 tranxactions: Tranxaction.filter(nil,
+                                                  end_of_financial_year_date - 1.year + 1.day,
+                                                  end_of_financial_year_date,
+                                                  nil,
+                                                  nil,
+                                                  true).where.not(tranxactable_type: %w[Client Property TenancyAgreement]),
+                 title: 'Other tranxactions'
+               }
+        render partial: 'active_admin/dashboard/tax_categories',
+               locals: {
+                 end_of_financial_year_date:
+               }
+      end
+    end
   end
 end
