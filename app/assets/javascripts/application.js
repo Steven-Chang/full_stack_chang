e9b0177afb90073081ab4f083f57dc398bf02641
Steven Chang <stevenchang5000@protonmail.com>
2023-11-17 07:50:08 +1000

    Init change to Velzon (#464)

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+import "@hotwired/turbo-rails";
+
+// === VELZON ===
+import "../../lib/assets/js/layout";
+import "../../lib/assets/js/app";
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,3 @@
+import "@hotwired/turbo-rails";
 import "./src/jquery";
 import "./src/bootstrap";
