40936065ddc8ac330ebbce22ca27dcf2090f1cde
Steven Chang <stevenchang5000@protonmail.com>
2023-11-18 16:18:22 +1000

    Remove hide footer and hide navbar (#475)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,45 +8,34 @@ class ApplicationController < ActionController::Base
   # === CALLBACKS ===
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # === HELPER METHODS ===
-  helper_method :hide_footer, :hide_navbar
-
   def home
     @page_title = 'Home'
   end
 
   protected
 
-  def after_sign_out_path_for(_resource_or_scope)
-    '/'
-  end
+    def after_sign_out_path_for(_resource_or_scope)
+      '/'
+    end
 
-  def configure_permitted_parameters
-    added_attrs = %i[
-      username
-    ]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:otp_attempt])
-  end
+    def configure_permitted_parameters
+      added_attrs = %i[
+        username
+      ]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:otp_attempt])
+    end
 
-  # use predefined method name
-  def authenticate_admin_user!
-    return access_denied if user_signed_in? && !current_user.admin?
+    # use predefined method name
+    def authenticate_admin_user!
+      return access_denied if user_signed_in? && !current_user.admin?
 
-    authenticate_user!
-  end
+      authenticate_user!
+    end
 
-  def access_denied
-    flash[:alert] = 'You are not authorized to perform this action.'
-    redirect_to(root_path)
-  end
-
-  def hide_footer
-    false
-  end
-
-  def hide_navbar
-    false
-  end
+    def access_denied
+      flash[:alert] = 'You are not authorized to perform this action.'
+      redirect_to(root_path)
+    end
 end
