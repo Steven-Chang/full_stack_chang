088e283a34013c3b11d6947a2b3d081c47710c86
Steven Chang <stevenchang5000@protonmail.com>
2023-11-29 18:40:19 +1000

    transactions show: init; transactiosn index: link to show; (#482)

--- app/controllers/tranxactions_controller.rb
+++ app/controllers/tranxactions_controller.rb
@@ -4,8 +4,12 @@ class TranxactionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @q = Tranxaction.ransack(params[:q])
+    @q = current_user.tranxactions.ransack(params[:q])
     @q.sorts = 'date desc' if @q.sorts.empty?
     @tranxactions = @q.result(distinct: true).page(params[:page])
+  end
+
+  def show
+    @tranxaction = current_user.tranxactions.find(params[:id])
   end
 end
