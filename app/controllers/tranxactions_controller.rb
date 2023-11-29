6ed0122505cb620c34ca51fca1bed2b80d14a6ca
Steven Chang <stevenchang5000@protonmail.com>
2023-11-29 17:49:06 +1000

    transactions index: enable ransack in controller and sorting by date (#481)

--- app/controllers/tranxactions_controller.rb
+++ app/controllers/tranxactions_controller.rb
@@ -3,5 +3,9 @@
 class TranxactionsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @q = Tranxaction.ransack(params[:q])
+    @q.sorts = 'date desc' if @q.sorts.empty?
+    @tranxactions = @q.result(distinct: true).page(params[:page])
+  end
 end
