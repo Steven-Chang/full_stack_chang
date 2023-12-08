4429cefbdbf45f7490b920fc238ad82c5754ca4d
Steven Chang <stevenchang5000@protonmail.com>
2023-12-08 12:52:59 +1000

    aa payment summaries: scope by user

--- app/models/payment_summary.rb
+++ app/models/payment_summary.rb
@@ -10,22 +10,26 @@ class PaymentSummary < ApplicationRecord
 	validates :client_id, uniqueness: { scope: :year_ending }
 	validates :year_ending, numericality: { only_integer: true }
 
+  # === SCOPES ===
+  scope :emily, lambda { User.find(14).payment_summaries }
+  scope :steven, lambda { User.find(1).payment_summaries }
+
   # === INSTANCE METHODS ===
 	# def gross_payment
- #    client.tranxactions
- #          .where(tax: true)
- #          .where('amount > ?', 0)
- #          .where('date >= ?', Date.new(year_ending - 1, 7, 1))
- #          .where('date < ?', Date.new(year_ending, 7, 1))
- #          .sum(:amount)
+  #    client.tranxactions
+  #          .where(tax: true)
+  #          .where('amount > ?', 0)
+  #          .where('date >= ?', Date.new(year_ending - 1, 7, 1))
+  #          .where('date < ?', Date.new(year_ending, 7, 1))
+  #          .sum(:amount)
 	# end
 
- #  def total_expenses
- #    client.tranxactions
- #          .where(tax: true)
- #          .where('amount < ?', 0)
- #          .where('date >= ?', Date.new(year_ending - 1, 7, 1))
- #          .where('date < ?', Date.new(year_ending, 7, 1))
- #          .sum(:amount)
- #  end
+  #  def total_expenses
+  #    client.tranxactions
+  #          .where(tax: true)
+  #          .where('amount < ?', 0)
+  #          .where('date >= ?', Date.new(year_ending - 1, 7, 1))
+  #          .where('date < ?', Date.new(year_ending, 7, 1))
+  #          .sum(:amount)
+  #  end
 end
