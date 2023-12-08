651df862a4ecfb891c7d5ea439e96b14cf52186c
Steven Chang <stevenchang5000@protonmail.com>
2023-12-09 07:34:03 +1000

    Creditor and Client: #admin_label (#517)

--- app/models/tax_category.rb
+++ app/models/tax_category.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class TaxCategory < ApplicationRecord
+  # === ALIAS ATTRIBUTE ===
+  alias_attribute :name, :description
+
 	# === ASSOCIATIONS ===
   has_many :tranxactions, dependent: :restrict_with_error
+
+  # === DEFAULT SCOPE ===
+  default_scope { order(:description) }
 
   # === VALIDATIONS ===
   validates :description, presence: true
