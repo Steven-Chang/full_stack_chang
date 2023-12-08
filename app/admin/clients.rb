37c969a13a386d4a0d3eae0e0aad6ffd66c88379
Steven Chang <stevenchang5000@protonmail.com>
2023-12-08 11:28:17 +1000

    aa clients: add user where applicable

--- app/admin/clients.rb
+++ app/admin/clients.rb
@@ -1,27 +1,31 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Client do
-  permit_params :email, :name
-
   # === MENU ===
   menu parent: 'Taxes'
 
+  # === INDEX ===
   index do
     selectable_column
     id_column
     column :email
     column :name
+    column :user
     actions
   end
 
   filter :email
   filter :name
 
+  # === FORM ===
   form do |f|
     f.inputs do
       f.input :email
       f.input :name
+      f.input :user
     end
     f.actions
   end
+
+  permit_params :email, :name, :user_id
 end
